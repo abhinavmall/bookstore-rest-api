37098a3c825aa581d7079df4e6ab57f5401b999c
Abhinav Mall <abhinavmall@users.noreply.github.com>
2025-01-27 21:36:14 +0530

    Routes integrated

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express'
 import config from 'config'
 import log from "./utils/logger";
 import connectToDb from "./utils/connectToDb";
+import routes from './routes'
+
 
 const port = config.get<number>('port')
 
@@ -9,5 +11,8 @@ const app = express()
 
 app.listen(port, () => {
     connectToDb();
+
+    routes(app);
+
     log.info(`Bookstore api started at http://localhost:${port}`);
 })
