10de0d2e5323c24230451f59151e471d1d08f532
Abhinav Mall <abhinavmall@users.noreply.github.com>
2025-01-27 23:42:34 +0530

    Session working

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,15 @@ import config from 'config'
 import log from "./utils/logger";
 import connectToDb from "./utils/connectToDb";
 import routes from './routes';
-
+import deserializeUser from './middleware/deserializeUser';
 
 const port = config.get<number>('port');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(deserializeUser);
 
 app.listen(port, () => {
     connectToDb();
